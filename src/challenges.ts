97ec42d2404fbd09198812971a47fc04a3c751ae
abimael <tavares@abimael.dev.br>
2022-08-23 21:59:44 -0300

    finished my test

--- src/challenges.ts
+++ src/challenges.ts
@@ -11,3 +11,61 @@ export const isOddOrEven = (value: number) => {
 
     return tempNumber === 0
 }
+
+// Question 2
+export const isProperlyNested = (value: string) => {
+    const nestedOpen: string[] = ['(', '{', '[']
+    const nestedClose: string[] = [')', '}', ']']
+    const {length} = value
+
+    if(length === 0) {
+        return true
+    }
+    if(nestedOpen.includes(value.charAt(0)) && nestedClose.includes(value.charAt(length - 1))) {
+        const indexOpen = nestedOpen.findIndex((item) => item === value.charAt(0))
+        const indexClose = nestedClose.findIndex((item) => item === value.charAt(length - 1))
+        return indexOpen === indexClose
+    }
+
+    return false
+}
+
+// Question 3
+export const quantityZeroInIntegerToBinary = (value: number) => {
+    const binaryNumber = Number(value).toString(2)
+    let countZero = 0
+    let current = 0
+    for (let i = 0; i<binaryNumber.length; i++) {
+        Number(binaryNumber[i]) === 1 ? current = 0 : current++
+        if (current > countZero) {
+            countZero = current
+        }
+    }
+    return countZero;
+
+}
+
+
+// Question 4
+export const rangeNumber = (valueX: number, ValueY:number) => {
+
+
+}
+
+
+// Question 5
+export const rotatedElement = (numbers: number[], valueN:number) => {
+    const result = [...numbers]
+   for (let i = 0; numbers.length; i++) {
+       if(valueN < 0) {
+          result[i] = result[i+1]
+           return
+       }
+
+       result[i+1] = result[i]
+   }
+
+   return result
+
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-import {isOddOrEven} from "./challenges";
+import {isOddOrEven, isProperlyNested, quantityZeroInIntegerToBinary} from "./challenges";
 
 console.log('the number 3 is Even?', isOddOrEven(3)) // false
 console.log('the number -1 is Even?', isOddOrEven(-1)) // false
 console.log('the number -2 is Even?',isOddOrEven(-2)) // true
 console.log('the number 0 is Even?',isOddOrEven(0)) // true
+
+console.log('it is has nested property?', isProperlyNested('{name}')) // true
+console.log('it is has nested property?', isProperlyNested('{[()()]}')) // true
+console.log('it is has nested property?', isProperlyNested('{name')) // false
+console.log('it is has nested property?', isProperlyNested('([)()]')) // false
+
+console.log('consecutive zeros in binary value?', quantityZeroInIntegerToBinary(9)) // 2
+console.log('consecutive zeros in binary value?', quantityZeroInIntegerToBinary(328)) // 2
+console.log('consecutive zeros in binary value?', quantityZeroInIntegerToBinary(20)) // 1
+
